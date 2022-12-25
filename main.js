c910369cc128cdecf6524b84cb5cf48d22da0544
Beletskyi-Moawad Martin <m.beleckii@alef.im>
2022-12-25 15:35:06 +0300

    Добавлены снежинки

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 const envelope = document.querySelector('.envelope');
 const body = document.querySelector('body');
+const overlay = document.querySelector('.redirect-overlay');
+const full_text = document.querySelector('.full-text');
 
 envelope.addEventListener('click', (e) => {
     e.preventDefault();
 
     envelope.classList.add('hidden');
+    overlay.classList.add('active');
     setTimeout(() => {
         body.classList.add('message');
-    }, 1000)
+        overlay.classList.remove('active');
+    }, 1000);
+
+    setTimeout(() => {
+        full_text.classList.add('active');
+    }, 1500);
 })
